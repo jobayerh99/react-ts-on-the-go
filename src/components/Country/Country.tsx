39fb32fafef5fa0fce2a456abf9370315e82de73
jobayerh99 <jobayerh99@gmail.com>
2026-09-12 01:32:04 +0600

    use state learnig and button toggole completew

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import type { CountryType } from "../../country"
 import './Country.css'
 export interface CountryProps {
@@ -5,14 +6,29 @@ export interface CountryProps {
 }
 
 export default function Country({ country }: CountryProps) {
+
+    const [visited, setVisited] = useState<Boolean>(false)
+
+    const handleVisited = () =>{
+        // setVisited(true)
+        // if(visited === true){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+        setVisited(!visited)
+    }
     
     return (
-        <div className="country">
+        <div className={`country ${visited? 'country-visited' : ''}`}>
             <h3>{country.name.common}</h3>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <p>Population:{} </p>
             <p>Capital:{} </p>
-            <button>Visited</button>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Mark as Visited'}
+            </button>
         </div>
     )
 }
