fd161de3cd2b61c8432d8f38b749e9400735fb80
jobayerh99 <jobayerh99@gmail.com>
2026-09-13 00:03:09 +0600

    all complete ready for deploy

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -11,23 +11,48 @@ export default function Countries({ countriesPromise }: CountriesProps) {
   const countries = use(countriesPromise);
   // console.log(countries);
 
-  const [visitedCountries, setVisitedCountries] = useState<CountryType[]>([])
+  const [visitedCountries, setVisitedCountries] = useState<CountryType[]>([]);
 
-  const handleVisitedCountries = (country:CountryType):void => {
-    const newVisitedCountries = [...visitedCountries, country]
-    setVisitedCountries(newVisitedCountries);
-  }
+  const handleVisitedCountries = (country: CountryType): void => {
+    // bad way to check array/object
+    // if (visitedCountries.includes(country)) {
+    // } else {
+    //   const newVisitedCountries = [...visitedCountries, country];
+    //   setVisitedCountries(newVisitedCountries);
+    // }
+
+    // good Way to check array/object
+    const exists = visitedCountries.find(
+      (c) => c.ccn3.ccn3 === country.ccn3.ccn3,
+    );
+    if (exists) {
+      const remainingCountries = visitedCountries.filter(
+        (c) => c.ccn3.ccn3 !== country.ccn3.ccn3,
+      );
+      setVisitedCountries(remainingCountries);
+    } else {
+      const newVisitedCountries = [...visitedCountries, country];
+      setVisitedCountries(newVisitedCountries);
+    }
+  };
 
   return (
     <div>
       <h2>Countries: {countries.length}</h2>
       <h4>Visited Countries: {visitedCountries.length}</h4>
+      <div>
+        <ul>
+          {visitedCountries.map((country) => (
+            <li key={country.ccn3.ccn3}>{country.name.common}</li>
+          ))}
+        </ul>
+      </div>
       <div className="countries">
         {countries.map((country) => (
-          <Country 
-          key={country.ccn3.ccn3} 
-          country={country}
-          handleVisitedCountries = {handleVisitedCountries}
+          <Country
+            key={country.ccn3.ccn3}
+            country={country}
+            handleVisitedCountries={handleVisitedCountries}
           ></Country>
         ))}
       </div>
